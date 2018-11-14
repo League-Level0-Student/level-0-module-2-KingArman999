933c199b3d7c7f1699196205f30e56142c60b5de
KingArman999 <league@wts-imac-11.attlocal.net>
2018-11-13 19:53:21 -0800

    Random MAgic?

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -1,4 +1,4 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
 package random;
@@ -13,16 +13,15 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		Random randy = new Random();
+		 r = randy.nextInt(133)+88;
 		//4. Limit the random number between 0 and 100
-
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+		 
 		//6. Challenge: Limit the random number between -222 and 88
 
 		//1. Print out the value of r
-		
+		 System.out.println(r);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
